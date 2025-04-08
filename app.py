027dfa7f7fda6ca860f04eb4d865ba386d50276d
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-04-08 20:33:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -722,7 +722,8 @@ with tab4:
     with col1:
         default_model = st.selectbox(
             "Default Chat Model",
-            ["gemini-1.5-flash", "gemini-2.0-flash", "gemini-2.0-flash-lite"],
+            ["gemini-2.0-flash", "gemini-1.5-flash", "gemini-1.5-pro","gemini-2.0-flash-lite","gemini-2.0-pro-exp-02-05",
+"gemini-2.0-flash-thinking-exp-01-21","gemini-2.5-pro-exp-03-25","gemini-1.5-flash-8b"],
             index=0
         )
         
